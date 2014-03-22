627731852fa342e01d6186cf25dc40da179212f0
Chris Bland <cbland@gmail.com>
2014-03-21 19:40:09 -0500

    helper update

--- routes/helpers.js
+++ routes/helpers.js
@@ -7,24 +7,30 @@ var dateFormatter = function(createdDate){
 exports.dateFormatter = dateFormatter;
 
 exports.variety = [
-	{label: 'Pinot Noir', value: 'Pinot Noir'},
+	{label: '', value: ''},
 	{label: 'Cabernet Sauvignon', value: 'Cabernet Sauvignon'},
-	{label: 'Malbec', value: 'Malbec'},
-	{label: 'Merlot', value: 'Merlot'},
-	{label: 'Syrah/Shiraz ', value: 'Syrah/Shiraz'},
-	{label: 'Zinfandel', value: 'Zinfandel'},
 	{label: 'Chardonnay', value: 'Chardonnay'},
 	{label: 'Gewurztraminer', value: 'Gewurztraminer'},
+	{label: 'Malbec', value: 'Malbec'},
+	{label: 'Merlot', value: 'Merlot'},
 	{label: 'Pinot Grigio', value: 'Pinot Grigio'},
+	{label: 'Pinot Noir', value: 'Pinot Noir'},
 	{label: 'Reisling', value: 'Reisling'},
 	{label: 'Sauvignon Blanc', value: 'Sauvignon Blanc'},
+	{label: 'Syrah/Shiraz ', value: 'Syrah/Shiraz'},
 	{label: 'Sémillon', value: 'Sémillon'},
-	{label: 'Viognier', value: 'Viognier'}
+	{label: 'Viognier', value: 'Viognier'},
+	{label: 'Zinfandel', value: 'Zinfandel'}
 ];
 
 exports.grapes = [
+	{label: '', value: ''},
+	{label: 'Champagne', 'Champagne'},
+	{label: 'Prosecco', value: 'Prosecco'}
 	{label: 'Red', value: 'Red'},
-	{label: 'White', value: 'White'}
+	{label: 'Rose', 'Rose'},
+	{label: 'Sparkling', 'Sparkling'},
+	{label: 'White', value: 'White'},
 ];
 
 var years = [];
